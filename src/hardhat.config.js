bc2997b1bbe5ceff6d9490e539ee8b819a8873c6
g6s <guayabas@hey.com>
2024-09-10 15:47:10 -0600

    foundry init along hardhat

--- src/hardhat.config.js
+++ src/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
+require("@nomicfoundation/hardhat-foundry");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
