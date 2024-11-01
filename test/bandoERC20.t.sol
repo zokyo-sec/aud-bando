57a2b123316386a4603581758b305f6f194068c2
Shanzson <shanzson@Shanzsons-Laptop.local>
2024-11-01 12:44:20 +0530

    Adding test setup

---
+++ test/bandoERC20.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {BandoERC20FulfillableV1} from "../contracts/BandoERC20FulfillableV1.sol";
+
+import {Upgrades} from "@openzeppelin-foundry-upgrades/Upgrades.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+
+contract BandoERC20FulfillableV1Test is Test {
+
+    BandoERC20FulfillableV1 public bandoERC20;
+    
+    address public _multiSigAddr;
+
+    address public bob = address(0x23);
+    address public alice = address(0x55);
+    address _proxy;
+    address _implementation;
+    address owner;
+
+
+    function setUp() public {
+        _multiSigAddr = address(0x22);
+        _initialMintTo = address(0x23);
+        _withdrawSigner = address(0x44);
+        address implementation = address(new BandoERC20FulfillableV1());
+
+        bytes memory data = abi.encodeCall(BandoERC20FulfillableV1.initialize);
+        address proxy = address(new ERC1967Proxy(implementation, data));
+        _proxy = proxy;
+        _implementation = implementation;
+        owner = _multiSigAddr;
+
+        bandoERC20 = BandoERC20FulfillableV1(proxy);
+        
+    }
+}
